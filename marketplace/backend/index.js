4bf4cda506e7197dbaf554766a639ed692144e43
eles17 <lev.starman8@gmail.com>
2025-02-12 21:22:20 +0100

    Implemented user authentication with JWT-based login and registration.Tested user authentication with Postman, verified JWT-based login & protected routes. Came a cross a typo that was later fixed.

--- marketplace/backend/index.js
+++ marketplace/backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pool = require('./config/db'); // import databes connection
+const authRoutes = require('./routes/authRoutes'); // import auth routes
 
 const app = express();
 app.use(cors());
@@ -12,8 +13,10 @@ app.get('/', (req,res) =>{
     res.send('Backend is working! PostgresSQL is connected.');
 });
 
+app.use('/api/auth', authRoutes);
+
 //test databse connection
-app.get('/test-db', async (req, res) => {
+/*app.get('/test-db', async (req, res) => {
     try{
         const result =await pool.query('SELECT NOW()');
         res.json({ message: "Database is working", time: result.rows[0].now });
@@ -21,7 +24,7 @@ app.get('/test-db', async (req, res) => {
         console.error("Database error:", err);
         res.status(500).json({ error: "Database connection failed"});
     }
-});
+});*/
 
 //Start server
 const PORT= process.env.PORT || 5001;
---
+++ marketplace/backend/routes/authRoutes.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { body, validationResult } =require('express-validator');
+const pool = require('../config/db');
+require('dotenv').config();
+
+const router = express.Router();
+
+//user registration
+router.post('/register',
+    [
+        body('email').isEmail().withMessage('Invalid email format'),
+        body('password').isLength({ min:6 }).withMessage('Password must be at least 6 characters')
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password, full_name, address } = req.body;
+
+        try { //check if the user already exists
+            const userExists = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+            if (userExists.rows.length > 0) {
+                return res.status(400).json({ message: "User already exsits"});
+            }
+
+            //hash password
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+
+            //insert new user
+            const newUser = await pool.query(
+                "INSERT INTO users (email, password_hash, full_name, address) VALUES ($1, $2, $3, $4) RETURNING id, email, full_name",
+                [email, hashedPassword, full_name, address]
+            );
+
+            res.status(201).json({ message: "User registered successfully", user: newUser.rows[0] });
+        }catch (err) {
+            console.error("Registration Error:", err);
+            res.status(500).json({ error: "Server error"});
+        }
+    }
+);
+
+//User login
+router.post('/login',
+    [
+        body('email').isEmail().withMessage('Invalid email format'),
+        body('password').exists().withMessage('Password is required')
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try { //check if the user exists
+            const user = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+            if (user.rows.length === 0) {
+                return res.status(400).json({ message: "Invalid email or password"});
+            }
+
+            //compare passwords
+            const validPassword = await bcrypt.compare(password, user.rows[0].password_hash);
+            if (!validPassword){
+                return res.status(400).json({ message: "Invalid email or password"});
+            }
+
+            //generate JWT token
+            const token = jwt.sign(
+                { id: user.rows[0].id, email: user.rows[0].email },
+                process.env.JWT_SECRET,
+                { expiresIn: "1h"}
+            );
+
+            res.json({ message: "Login successful", token });
+        }catch (err) {
+            console.error("Login Error:", err);
+            res.status(500).json({ error: "Server error"});
+        }
+    }
+);
+module.exports = router; 
