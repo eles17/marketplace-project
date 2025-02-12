b36873ddd8ccf4e1bd12a72597ec8c1725ade4ec
eles17 <lev.starman8@gmail.com>
2025-02-12 18:26:01 +0100

    Implemented module-based structure for Marketplace with submodules renamed them for consistant structure.I have routed them between each other.

--- marketplace/frontend/src/app/marketplace/marketplace-routing.module.ts
+++ marketplace/frontend/src/app/marketplace/marketplace-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'retail', loadChildren: () => import('./retail/retail.module').then(m => m.RetailModule)},
+  { path: 'vehicle', loadChildren: () => import('./vehicles/vehicles.module').then(m => m.VehiclesModule)},
+  { path: 'real-estate', loadChildren: () => import('./real-estate/real-estate.module').then(m => m.RealEstateModule)},
+  { path: '', redirectTo: '/auth/login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
