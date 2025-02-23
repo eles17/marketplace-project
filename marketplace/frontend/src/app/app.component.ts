3d213aabc52ed8e8cc96aa301be383f4357d65da
eles17 <lev.starman8@gmail.com>
2025-02-23 01:28:14 +0100

    Imporved app.component html and ts

--- marketplace/frontend/src/app/app.component.ts
+++ marketplace/frontend/src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
 
 @Component({
   selector: 'app-root',
   standalone: false,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/auth/login']);
+  }
 }
